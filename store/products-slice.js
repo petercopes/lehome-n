5e08e0bf61b73e73915d082eea80d802400cf529
Pedro Copes <petercopes.96@gmail.com>
2021-12-20 14:50:47 -0300

    corrected product-slice

--- store/products-slice.js
+++ store/products-slice.js
@@ -8,23 +8,6 @@ const productSlice = createSlice({
     setProducts(state, action) {
       state.productList = action.payload;
     },
-    removeFromCart(state, action) {
-      state.totalItems = state.totalItems - action.payload.amount;
-      state.items = state.items.filter((e) => e.id !== action.payload.id);
-    },
-    updateAmountOnCart(state, action) {
-      const productToUpdateIndex = state.items.findIndex(
-        (e) => e.id === action.payload.id
-      );
-      state.totalItems =
-        state.totalItems +
-        (action.payload.amount - state.items[productToUpdateIndex].amount);
-      state.items[productToUpdateIndex].amount = action.payload.amount;
-    },
-    emptyCart(state, action) {
-      state.items = [];
-      state.totalItems = 0;
-    },
   },
 });
 export const ProductActions = productSlice.actions;

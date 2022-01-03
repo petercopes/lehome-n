8ae70ced37ff7b7b566ad1f0cdc16e8815058dc2
Pedro Copes <petercopes.96@gmail.com>
2022-01-03 16:35:01 -0300

    images and icos

--- screens/Home.jsx
+++ screens/Home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   View,
   StyleSheet,
@@ -33,19 +33,70 @@ const styles = StyleSheet.create({
   },
 });
 const Home = () => {
+  const [image, setImage] = useState(
+    "https://static.zara.net/photos///2021/S/0/2/p/0706/367/803/2/w/750/0706367803_2_1_1.jpg?ts=1633428472209"
+  );
+  const handleNext = () => {
+    if (
+      image ===
+      "https://static.zara.net/photos///2021/S/0/2/p/0706/367/803/2/w/750/0706367803_2_1_1.jpg?ts=1633428472209"
+    ) {
+      setImage(
+        "https://static.zara.net/photos///2021/I/L/2/p/2109/212/000/2/w/750/2109212000_1_1_1.jpg?ts=1631694929445"
+      );
+    } else if (
+      image ===
+      "https://static.zara.net/photos///2021/I/L/2/p/2109/212/000/2/w/750/2109212000_1_1_1.jpg?ts=1631694929445"
+    ) {
+      setImage(
+        "https://static.zara.net/photos///2021/I/0/1/p/6361/651/800/12/w/750/6361651800_2_1_1.jpg?ts=1636538059410"
+      );
+    } else {
+      setImage(
+        "https://static.zara.net/photos///2021/S/0/2/p/0706/367/803/2/w/750/0706367803_2_1_1.jpg?ts=1633428472209"
+      );
+    }
+  };
+  const handlePrevious = () => {
+    if (
+      image ===
+      "https://static.zara.net/photos///2021/S/0/2/p/0706/367/803/2/w/750/0706367803_2_1_1.jpg?ts=1633428472209"
+    ) {
+      setImage(
+        "https://static.zara.net/photos///2021/I/0/1/p/6361/651/800/12/w/750/6361651800_2_1_1.jpg?ts=1636538059410"
+      );
+    } else if (
+      image ===
+      "https://static.zara.net/photos///2021/I/L/2/p/2109/212/000/2/w/750/2109212000_1_1_1.jpg?ts=1631694929445"
+    ) {
+      setImage(
+        "https://static.zara.net/photos///2021/S/0/2/p/0706/367/803/2/w/750/0706367803_2_1_1.jpg?ts=1633428472209"
+      );
+    } else {
+      setImage(
+        "https://static.zara.net/photos///2021/I/L/2/p/2109/212/000/2/w/750/2109212000_1_1_1.jpg?ts=1631694929445"
+      );
+    }
+  };
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Welcome to our Website</Text>
       <View style={styles.imageContainer}>
         <TouchableOpacity>
-          <Ionicons name="arrow-back-outline" size={32} color={"black"} />
+          <Ionicons
+            name="arrow-back-outline"
+            size={32}
+            color={"black"}
+            onPress={handlePrevious}
+          />
         </TouchableOpacity>
-        <Image
-          style={styles.mainImage}
-          source={require("../assets/images/home1.jpg")}
-        />
+        <Image style={styles.mainImage} source={{ uri: image }} />
         <TouchableOpacity>
-          <Ionicons name="arrow-forward-outline" size={32} />
+          <Ionicons
+            name="arrow-forward-outline"
+            size={32}
+            onPress={handleNext}
+          />
         </TouchableOpacity>
       </View>
     </View>
